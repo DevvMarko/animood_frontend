1b90dc18eabd71857f62f7ab79c344cc093b7523
DevDaru <devvmarko@gmail.com>
2025-07-06 17:09:12 +0200

    change structure of header and units in css

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,20 +4,21 @@ import styles from './header.module.css';
 
 function Header() {
     return (
-        <>
-            <header className={styles.header}>
+        <header className={styles.header}>
+            <section className={styles.mainContent}>
                 <Link to="/" className={styles.logo}>
-                    <h1>ANIMOOD</h1>
-                </Link>
-                <div className={styles.actionsContainer}>
-                    <Link to="/add" className={styles.addButton}>
-                        Dodaj anime
+                        <h1>ANIMOOD</h1>
                     </Link>
-                </div>
-                
-            </header>
+                    <div className={styles.actionsContainer}>
+                        <Link to="/add" className={styles.addButton}>
+                            Dodaj anime
+                        </Link>
+                    </div>
+            </section>
+            
             <div className={styles.headerLine}></div>
-        </>
+        </header>
+
     );
-    }
+}
 export default Header;
