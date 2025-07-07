30a49c9df466c44dd075bf25830fa16b6a425896
DevDaru <devvmarko@gmail.com>
2025-07-07 15:25:28 +0200

    add hook to get header height

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import React from 'react';
+import React, { useRef } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 // components
@@ -13,17 +13,27 @@ import EditAnimePage from './pages/EditAnimePage.jsx';
 
 
 function App() {
+  const headereRef = useRef(null);
+  const [headerHeight, setHeaderHeight] = React.useState(0);
+
+  React.useEffect(() => {
+    // Ten kod zostanie uruchomiony po zamontowaniu komponentu
+    if (headereRef.current) {
+      setHeaderHeight(headereRef.current.offsetHeight);
+    }
+  }, []); // Pusta tablica zależności sprawia, że hook uruchomi się tylko raz po początkowym renderowaniu
+
   return (
     <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/add" element={<AddAnimePage />} />
-          <Route path="/anime/:id" element={<AnimeDetailsPage />} />
-          <Route path="/edit/:id" element={<EditAnimePage />} />
-          {/* Możesz dodać więcej tras tutaj */}
-        </Routes>
-      </Router>
+      <Header ref={headereRef}/>
+      <Routes>
+        <Route path="/" element={<MainPage headerHeightRef={headerHeight}/>} />
+        <Route path="/add" element={<AddAnimePage />} />
+        <Route path="/anime/:id" element={<AnimeDetailsPage />} />
+        <Route path="/edit/:id" element={<EditAnimePage />} />
+
+      </Routes>
+    </Router>
   );
 }
 
